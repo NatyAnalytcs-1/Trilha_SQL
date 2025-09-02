82fb6668513e425b41eb30277cad683f90699967
Naty Anastacio <147287067+NatyAnalytcs-1@users.noreply.github.com>
2025-09-02 09:44:39 -0300

    Create where.sql

---
+++ where.sql
@@ -0,0 +1,32 @@
+--Retorna todos os registros da tabela SalesLT.Address
+--Onde a cidade (City) é Toronto
+--E o ano do campo ModifiedDate é 2005
+
+
+SELECT * FROM 
+	SalesLT.Address AS AD
+WHERE
+	AD.City = 'Toronto'
+	END
+	Year(AD.ModifiedDate) = 2005
+
+
+
+
+--Retorna registros que satisfaçam qualquer uma destas condições:
+--Cidade = Toronto e ano de ModifiedDate = 2005
+--Cidade = Round Rock (independente da data)
+--Cidade = Richmond (independente da data)
+
+SELECT * FROM 
+	SalesLT.Address AS AD
+WHERE
+	(
+		AD.City = 'Toronto'
+		AND
+		Year(AD.ModifiedDate) = 2005
+	)
+	OR
+	AD.City = 'Round Rock'
+	OR
+	AD.City = 'Richmond'
